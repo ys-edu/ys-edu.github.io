f4a5452120e702816100be162ae66e3474265630
Ysabelle <yskim@csumb.edu>
2026-03-24 02:34:44 -0700

    hw 3 mostly working again x2

--- hw3/js/dex.js
+++ hw3/js/dex.js
@@ -1,63 +1,51 @@
-// event listeners for nickname, dexNum
-document.querySelector("#nickname").addEventListener("change", validateNickname);
-document.querySelector("#dexNum").addEventListener("change", displayPKMN);
-document.querySelector("#dexNum").addEventListener("change", validateNum);
-document.querySelector("#signupForm").addEventListener("submit", function(event) {
-    validateNum(event);
+// event listeners for pkmnButton, nickname, dexNum
+document.querySelector("#pkmnForm").addEventListener("submit", function(event) {
+    validateNickname(event);
 });
+document.querySelector("#pkmnButton").addEventListener("click", displayPKMN);
+document.querySelector("#nickname").addEventListener("change", validateNickname);
+
+// initialize global variables
+let randomNum;
+// hide Pokémon stat block until button is pressed
+document.querySelector("#pkmnForm").style.display = "none";
 
 // resets nicknameError and dexNumberError to blank
 document.querySelector("#nicknameError").innerHTML = "";
-document.querySelector("#dexNumberError").innerHTML = "";
+
+// displayPKMN() fetches data from web API and displays information after user enters a valid number
+async function displayPKMN() {
+    // display pkmnBlock info
+    document.querySelector("#pkmnForm").style.display = "grid";
+    // random number from 1 to 1025
+    randomNum = Math.floor(Math.random() * 1025) + 1;
+    // console.log("Random number: "+randomNum); testing
+    let url = `https://pokeapi.co/api/v2/pokemon/${randomNum}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    let pkmnSpriteImg = document.querySelector("#pkmnSprite");
+    pkmnSpriteImg.src = data.sprites.front_default;
+    document.querySelector("#pkmnNum").innerHTML = data.id;
+    let pkmnName = data.name;
+    document.querySelector("#pkmnName").innerHTML =pkmnName.charAt(0).toUpperCase() + pkmnName.slice(1);
+    document.querySelector("#pkmnTypes").innerHTML = data.types[0].type[0];
+}
 
 // validateNickname() checks if the nickname is between 1-10 characters long
-function validateNickname() {
+function validateNickname(e) {
     let isValid = true;
     let nickname = document.querySelector("#nickname").value;
     document.querySelector("#nicknameError").innerHTML = "";
     if (nickname.length > 10) {
-        document.querySelector("#usernameError").innerHTML = "Nickname is too long!";
+        document.querySelector("#nicknameError").innerHTML = "Nickname is too long!";
         isValid = false;
     }
     else if (nickname.length < 1) {
-        document.querySelector("#passwordError").innerHTML = "Nickname must be 1-10 characters long!";
+        document.querySelector("#nicknameError").innerHTML = "Nickname must be 1-10 characters long!";
         isValid = false;
     }
     if (isValid == false) {
         e.preventDefault();
-    }
-}
-
-// displayPKMN() fetches data from web API and displays information after user enters a valid number
-async function displayPKMN() {
-    let dexNumber = document.querySelector("#dexNum").value;
-    let url = `https://pokeapi.co/api/v2/pokemon/${dexNumber}`;
-    let response = await fetch(url);
-    let data = await response.json();
-    document.querySelector("#pkmnSprite").innerHTML = data.sprites.front_default;
-    document.querySelector("#pkmnName").innerHTML = data.name.toUpperCase();
-    document.querySelector("#pkmnTypes").innerHTML = data.types;
-}
-
-function validateNum(e) {
-    let isValid = true;
-    let username = document.querySelector("#username").value;
-    let password = document.querySelector("#pw").value;
-    let retypePassword = document.querySelector("#retypePW").value;
-    document.querySelector("#passwordError").innerHTML = "";
-    if (username.length == 0) {
-        document.querySelector("#usernameError").innerHTML = "Username is required!";
-        isValid = false;
-    }
-    else if (password.length < 6) {
-        document.querySelector("#passwordError").innerHTML = "Password must be at least 6 characters long!";
-        isValid = false;
-    }
-    else if (retypePassword != password) {
-        document.querySelector("#passwordError").innerHTML = "Passwords do not match!";
-        isValid = false;
-    }
-    if (isValid == false) {
-        e.preventDefault();
+        e.stopImmediatePropagation();
     }
 }
