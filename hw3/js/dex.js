4002e79a32c07cac69c595d32c614d9cd5777c5f
Ysabelle <yskim@csumb.edu>
2026-03-24 02:34:44 -0700

    hw 3 mostly working

--- hw3/js/dex.js
+++ hw3/js/dex.js
@@ -1,14 +1,35 @@
-// event listeners for nickname, dexNum
+// event listeners for pkmnButton, nickname, dexNum
+document.querySelector("#pkmnButton").addEventListener("click", displayPKMN);
 document.querySelector("#nickname").addEventListener("change", validateNickname);
-document.querySelector("#dexNum").addEventListener("change", displayPKMN);
-document.querySelector("#dexNum").addEventListener("change", validateNum);
-document.querySelector("#signupForm").addEventListener("submit", function(event) {
-    validateNum(event);
+document.querySelector("#pkmnForm").addEventListener("submit", function(event) {
+    validateNickname(event);
 });
+
+// initialize global variables
+let randomNum;
+// hide Pokemon stat block until button is pressed
+document.querySelector("#pkmnForm").style.display = "none";
 
 // resets nicknameError and dexNumberError to blank
 document.querySelector("#nicknameError").innerHTML = "";
-document.querySelector("#dexNumberError").innerHTML = "";
+
+// displayPKMN() fetches data from web API and displays information after user enters a valid number
+async function displayPKMN() {
+    // display pkmnBlock info
+    document.querySelector("#pkmnForm").style.display = "grid";
+    // random number from 1 to 1025
+    randomNum = Math.floor(Math.random() * 1025) + 1;
+    // console.log("Random number: "+randomNum); testing
+    let url = `https://pokeapi.co/api/v2/pokemon/${randomNum}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    pkmnSpriteImg = document.querySelector("#pkmnSprite");
+    pkmnSpriteImg.src = data.sprites.front_default;
+    document.querySelector("#pkmnNum").innerHTML = data.id;
+    pkmnName = data.name;
+    document.querySelector("#pkmnName").innerHTML =pkmnName.charAt(0).toUpperCase() + pkmnName.slice(1);
+    document.querySelector("#pkmnTypes").innerHTML = data.types[0].type;
+}
 
 // validateNickname() checks if the nickname is between 1-10 characters long
 function validateNickname() {
@@ -26,17 +47,6 @@ function validateNickname() {
     if (isValid == false) {
         e.preventDefault();
     }
-}
-
-// displayPKMN() fetches data from web API and displays information after user enters a valid number
-async function displayPKMN() {
-    let dexNumber = document.querySelector("#dexNum").value;
-    let url = `https://pokeapi.co/api/v2/pokemon/${dexNumber}`;
-    let response = await fetch(url);
-    let data = await response.json();
-    document.querySelector("#pkmnSprite").innerHTML = data.sprites.front_default;
-    document.querySelector("#pkmnName").innerHTML = data.name.toUpperCase();
-    document.querySelector("#pkmnTypes").innerHTML = data.types;
 }
 
 function validateNum(e) {
